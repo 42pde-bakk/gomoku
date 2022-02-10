c6d914e722e0831ca94d9b33803c5e1cecd66528
pde-bakk <pde-bakk@student.codam.nl>
2022-02-10 12:52:47 +0100

    figured out python3 allows unicode characters as variable names, so changed alpha and beta to α and β

--- srcs/minimax.py
+++ srcs/minimax.py
@@ -7,7 +7,7 @@ from srcs.gamestate import Gamestate
 class Minimax:
 	transposition_table = {}
 
-	def __init__(self, maxdepth: int = 3):
+	def __init__(self, maxdepth: int = 2):
 		self.maxdepth = maxdepth
 		# self.alpha, self.beta = -inf, inf
 
@@ -41,7 +41,7 @@ class Minimax:
 		assert best_state is not None
 		return best_value, best_state
 
-	def alphabeta(self, state: Gamestate, depth: int, alpha: float, beta: float, maximizing_player: bool) -> tuple[int, Gamestate]:
+	def alphabeta(self, state: Gamestate, depth: int, α: float, β: float, maximizing_player: bool) -> tuple[int, Gamestate]:
 		# Minimax.add_to_transp_table(gamestate)
 		if depth == 0 or state.winner is not None:
 			return state.h, state
@@ -49,28 +49,22 @@ class Minimax:
 		if maximizing_player:
 			best_value, best_state = -np.inf, None
 			for child in state.children:
-				value, state = self.alphabeta(child, depth - 1, alpha, beta, False)
-				# print(f'+.at depth {depth}, alphabeta returned value {value}')
+				value, state = self.alphabeta(child, depth - 1, α, β, False)
 				if value > best_value:
 					best_value, best_state = value, state
-					# print(f'{value} > {best_value}')
-				if best_value >= beta:
-					print(f'beta cutoff, {value} >= {beta}')
-					break  # Beta cutoff
-				alpha = max(alpha, value)
-				# print(f'updated alpha={alpha}')
+				if best_value >= β:
+					break  # β cutoff
+				α = max(α, value)
 			assert state is not None
 			return best_value, best_state
 		else:
 			best_value, best_state = np.inf, None
 			for child in state.children:
-				value, state = self.alphabeta(child, depth - 1, alpha, beta, True)
-				# print(f'-.at depth {depth}, alphabeta returned value {value}')
+				value, state = self.alphabeta(child, depth - 1, α, β, True)
 				if value < best_value:
 					best_value, best_state = value, state
-				if best_value <= alpha:
-					print(f'alpha cutoff, {value} <= {alpha}')
-					break  # Alpha cutoff
-				beta = min(beta, value)
+				if best_value <= α:
+					break  # α cutoff
+				β = min(β, value)
 			assert state is not None
 			return best_value, best_state
