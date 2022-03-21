c3d96d0834c754a94687e43deb43492dacceefc2
pde-bakk <pde-bakk@student.codam.nl>
2022-03-21 14:18:14 +0100

    Fix: forgot to update tests to match update_h syntax

--- tests/test_gamestate.py
+++ tests/test_gamestate.py
@@ -20,7 +20,7 @@ class TestGamestate(TestCase):
 		gamestate.board.arr[(10, 0)] = 1
 		og_h = gamestate.set_h()
 		gamestate.board.arr[(8, 0)] = 1
-		h = gamestate.update_h(pos = (8, 0))
+		h = gamestate.update_h(pos = (8, 0), player = 0)
 		print(f'h={h}')
 		assert h == og_h - 20 + 10000
 
@@ -28,14 +28,14 @@ class TestGamestate(TestCase):
 		gamestate = Gamestate()
 		for move, item in zip([(7, 8), (9, 10), (8, 9)], [1, 2, 1]):
 			gamestate.board.arr[move] = item
-			updated_h = gamestate.update_h(pos = move)
+			updated_h = gamestate.update_h(pos = move, player = item - 1)
 			set_h = gamestate.set_h()
 			assert updated_h == set_h
 
 	def test_same_2(self):
 		gamestate = Gamestate()
 		for move, item in zip([(9, 9), (7, 8), (8, 10), (8, 9)], [1, 2, 1, 2]):
-			gamestate.place_stone(y = move[0], x = move[1], stone = item)
+			gamestate.place_stone(y = move[0], x = move[1], player = item - 1)
 			updated_h = gamestate.h
 			set_h = gamestate.set_h()
 			assert updated_h == set_h
