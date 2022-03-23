f669cf724912d68db28e644b5d462988feb92d20
pde-bakk <pde-bakk@student.codam.nl>
2022-03-23 16:29:20 +0100

    try catch these hands

--- algo/srcs/main.cpp
+++ algo/srcs/main.cpp
@@ -6,18 +6,28 @@
 #include "Gamestate.hpp"
 #include "Server.hpp"
 #include "Client.hpp"
+#include "Colours.hpp"
 
 int main() {
-	Server server;
-	Client	client(&server);
+	try {
+		Server server;
+		std::cerr << "Welcome to Gomokubot! Connect on port " << server.getport() << "\n";
+		std::cerr << "Waiting for client to connect\n";
+		Client	client(&server);
 
-	std::cerr << "Welcome to Gomokubot!\n";
-
-	while (true) {
-		Gamestate* gs = client.receiveGamestate();
-		gs->print_board(std::cerr);
-		Move move = get_random_move(gs);
-		std::cerr << "Move: " << move;
-		client.send_move(move);
+		while (true) {
+			std::cerr << "Lets receive a gamstate\n";
+			Gamestate* gs = client.receiveGamestate();
+			std::cerr << "got the gamestate\n";
+			gs->print_board(std::cerr);
+			gs->generate_children();
+			Move move = get_random_move(gs);
+			std::cerr << "Move: " << move;
+			client.send_move(move);
+		}
 	}
+	catch (std::exception& e) {
+		std::cerr << _BLUE _BOLD << e.what() << _END << '\n';
+	}
+	exit(1);
 }
