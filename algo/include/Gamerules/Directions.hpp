8ed98b6a17e3d41304d52ae867d878121e881db4
pde-bakk <pde-bakk@student.codam.nl>
2022-03-30 13:31:18 +0200

    doubled shift sizes

--- algo/include/Gamerules/Directions.hpp
+++ algo/include/Gamerules/Directions.hpp
@@ -9,13 +9,13 @@
 
 #define SHIFT_N(arg) (arg << BOARDWIDTH)
 #define SHIFT_S(arg) (arg >> BOARDWIDTH)
-#define SHIFT_W(arg) (arg << 1)
-#define SHIFT_E(arg) (arg >> 1)
+#define SHIFT_W(arg) (arg << 2)
+#define SHIFT_E(arg) (arg >> 2)
 
-#define SHIFT_NE(arg) (arg << (BOARDWIDTH - 1))
-#define SHIFT_SE(arg) (arg >> (BOARDWIDTH + 1))
-#define SHIFT_NW(arg) (arg << (BOARDWIDTH + 1))
-#define SHIFT_SW(arg) (arg >> (BOARDWIDTH - 1))
+#define SHIFT_NE(arg) (arg << (BOARDWIDTH - 2))
+#define SHIFT_SE(arg) (arg >> (BOARDWIDTH + 2))
+#define SHIFT_NW(arg) (arg << (BOARDWIDTH + 2))
+#define SHIFT_SW(arg) (arg >> (BOARDWIDTH - 2))
 
 std::array<int, 8>	all_dirs();
 std::array<int, 4>	setup_dirs();
