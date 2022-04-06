54c81d750b9b9de7c3304b78e0c87ccadbe57a69
pde-bakk <pde-bakk@student.codam.nl>
2022-04-06 12:44:18 +0200

    fixed hashing tests exceeding max captures

--- algo/tests/srcs/HashTest.cpp
+++ algo/tests/srcs/HashTest.cpp
@@ -12,6 +12,7 @@
 const int middle_idx = 9 * 20 + 9;
 
 void	place_random_stone(Gamestate *gs) {
+	unsigned int player = 0;
 	std::random_device dev;
 	std::mt19937 rng(dev());
 	std::uniform_int_distribution<std::mt19937::result_type> dist6(0,REALBOARDSIZE - 1); // distribution in range [0, 720]
@@ -19,24 +20,23 @@ void	place_random_stone(Gamestate *gs) {
 		unsigned int random = dist6(rng);
 		if (gs->tile_is_empty(random) && !Bitboard::isSeperatingBitIndex(random)) {
 //			std::cerr << "placed stone on idx " << random << '\n';
-			gs->place_stone(random);
+			gs->set(random, player);
+			player = !player;
 			break;
 		}
 	}
 }
 
 TEST_CASE("Bitset hash", "[HashingTests]") {
-	auto *gs = new Gamestate();
-
 	auto start_time = std::chrono::steady_clock::now();
 	std::hash<bitboard> hash_fn;
 	for (int i = 0; i < 720; i++) {
-		place_random_stone(gs);
-		auto hash = hash_fn(gs->get());
+		Gamestate gs;
+		place_random_stone(&gs);
+		auto hash = hash_fn(gs.get());
 //		std::cerr << "hash = " << hash << std::endl;
 	}
 	auto end_time = std::chrono::steady_clock::now();
 	auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
 	std::cerr << _PURPLE "hashing 720x took " << elapsed_time.count() << " ms\n" _END;
-	delete gs;
 }
