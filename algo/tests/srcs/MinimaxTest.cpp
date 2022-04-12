da1e815fd5bd369810976cae7c067cec5820c730
pde-bakk <pde-bakk@student.codam.nl>
2022-04-12 21:10:37 +0200

    figured out my minimaxtests were failing because the threads wouldn't always guarantee the same child-node to be first in the children vector, fixed it by disabling threading for my MinimaxTests

--- algo/tests/srcs/MinimaxTest.cpp
+++ algo/tests/srcs/MinimaxTest.cpp
@@ -2,13 +2,14 @@
 // Created by Peer De bakker on 3/25/22.
 //
 
-//#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_MAIN
 #include "../catch.hpp"
 #include "Gamestate.hpp"
 #include "Minimax.hpp"
 #include "Colours.hpp"
 
 const int middle_idx = 9 * 20 + 9;
+const int DEPTH = 1;
 
 void	place_stones(Gamestate *gs) {
 	int indices[] = {middle_idx, middle_idx + 1, middle_idx + 2};
@@ -22,11 +23,12 @@ TEST_CASE("Basic Minimax test", "[MinimaxTests]") {
 	place_stones(gs);
 
 	start_time = std::chrono::steady_clock::now();
-	Gamestate *minimax_res = minimax(gs, 2, gs->get_player());
+	Gamestate *minimax_res = minimax(gs, DEPTH, gs->get_player());
 	REQUIRE(minimax_res);
 	auto end_time = std::chrono::steady_clock::now();
 	auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
 	std::cerr << _PURPLE "Minimax took " << elapsed_time.count() << " ms. to find " << minimax_res << "\n" _END;
+	std::cerr << "Minimax returned:\n" << *minimax_res << "\n";
 
 	delete gs;
 }
@@ -36,7 +38,7 @@ TEST_CASE("Alphabeta Test", "[MinimaxTests]") {
 	place_stones(gs);
 
 	start_time = std::chrono::steady_clock::now();
-	Gamestate *ab_res = alphabeta(gs, 2, gs->get_player());
+	Gamestate *ab_res = alphabeta(gs, DEPTH, gs->get_player());
 	REQUIRE(ab_res);
 	auto end_time = std::chrono::steady_clock::now();
 	auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
@@ -49,21 +51,25 @@ TEST_CASE("Compare minimax with AB-pruning", "[MinimaxTests]") {
 
 	place_stones(gs);
 	place_stones(gs2);
+	std::cerr << "original:\n" << *gs << '\n';
 
 	start_time = std::chrono::steady_clock::now();
-	Gamestate *minimax_res = minimax(gs, 2, gs->get_player());
+	Gamestate *minimax_res = minimax(gs, DEPTH, gs->get_player());
 	auto end_time = std::chrono::steady_clock::now();
 	auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
 	std::cerr << _PURPLE "Minimax took " << elapsed_time.count() << " ms\n" _END;
 	minimax_res->print_heuristic(std::cerr);
 
 	start_time = std::chrono::steady_clock::now();
-	Gamestate *ab_res = alphabeta(gs2, 2, gs2->get_player());
+	Gamestate *ab_res = alphabeta(gs2, DEPTH, gs2->get_player());
 	end_time = std::chrono::steady_clock::now();
 	elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
 	std::cerr << _PURPLE "Alpha-Beta pruning took " << elapsed_time.count() << " ms\n" _END;
 	ab_res->print_heuristic(std::cerr);
 
+	std::cerr << "minimax_res:\n" << *minimax_res;
+	std::cerr << "ab_res:\n" << *ab_res;
+	std::cerr << "threaded: " << THREADED << "\n";
 	REQUIRE(*minimax_res == *ab_res);
 	delete gs; delete gs2;
 }
