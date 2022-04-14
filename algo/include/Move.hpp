1c3bb9b2fdc04914c3132495cd626b6c61ef819d
pde-bakk <pde-bakk@student.codam.nl>
2022-04-14 17:44:52 +0200

    gave Move a boolean to determine whether it is a tacticalMove or a quiet one

--- algo/include/Move.hpp
+++ algo/include/Move.hpp
@@ -12,10 +12,11 @@ class Move {
 public:
 	int	move_idx,
 		player;
+	bool tacticalMove;
 
 	// Allows the operator<< to access the private vars
 	friend std::ostream&	operator<<(std::ostream& o, const Move& m);
-	Move(int idx, int player);
+	Move(int idx, int player, bool isTactical = false);
 };
 
 
--- algo/srcs/Move.cpp
+++ algo/srcs/Move.cpp
@@ -5,7 +5,7 @@
 #include "Move.hpp"
 #include "Gamestate.hpp"
 
-Move::Move(int idx, int player) : move_idx(idx), player(player) {
+Move::Move(int idx, int player, bool isTactical) : move_idx(idx), player(player), tacticalMove(isTactical) {
 }
 
 std::ostream&	operator<<(std::ostream& o, const Move& m) {
