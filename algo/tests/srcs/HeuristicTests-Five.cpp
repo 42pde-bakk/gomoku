ca9dff441cc58ceebcacbb13dde2ad90a9a38563
pde-bakk <pde-bakk@student.codam.nl>
2022-07-21 19:27:15 +0200

    made extra test case for enclosed half open 4's with a space inbetween

--- algo/tests/srcs/HeuristicTests-Five.cpp
+++ algo/tests/srcs/HeuristicTests-Five.cpp
@@ -46,3 +46,34 @@ TEST_CASE_METHOD(Gamestate, "Win by blocked ive", "[HeuristicTests]") {
 	this->clear_children();
 	// TODO: ensure this always happens
 }
+
+TEST_CASE_METHOD(Gamestate, "Bogus", "[HeuristicTests]") {
+	this->set(middle_idx, 0);
+	this->set(middle_idx + EAST, 0);
+	this->set(middle_idx + 3 * EAST, 0);
+	this->set(middle_idx + NORTHWEST, 0);
+	this->set(middle_idx + SOUTH + 2 * WEST, 0);
+	this->set(middle_idx + SOUTH + 2 * EAST, 0);
+
+	this->set(middle_idx + NORTH, 1);
+	this->set(middle_idx + 2 * EAST, 1);
+	this->set(middle_idx + NORTH + 2 * EAST, 1);
+	this->set(middle_idx + NORTH + 3 * EAST, 1);
+	this->set(middle_idx + NORTH + 4 * EAST, 1);
+	this->set(middle_idx + 2 * NORTH + 2 * EAST, 1);
+
+
+	this->place_stone(middle_idx + NORTH + 5 * EAST);
+
+//	this->generate_children();
+	std::cerr << *this;
+	this->print_heuristic(std::cerr);
+//	std::cerr << "\n\n\n";
+//	for (auto child : children) {
+//		std::cerr << *child;
+//		child->print_heuristic(std::cerr);
+//		std::cerr << "\n";
+//	}
+	REQUIRE(this->values[1][HALF_OPEN_FOUR] == 1);
+
+}
